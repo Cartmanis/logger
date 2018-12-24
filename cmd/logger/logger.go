c0b0daa0bcbef0a1fa2890af27e852b3ff03c9af
cartmanis <vshmelcer@gmail.com>
2018-12-24 09:06:50 +0700

    first commit

--- cmd/logger/logger.go
+++ cmd/logger/logger.go
@@ -19,32 +19,41 @@ const (
 )
 
 type Logger struct {
-	OutToFile    bool
+	outToFile    bool
 	log          *log.Logger
 	file         *os.File
 	outToConsole bool
 }
 
-func NewLogger(path string, outToConsole bool) (*Logger, error) {
-	l := Logger{OutToFile: true}
-	logFile, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
-	l.file = logFile
-	l.outToConsole = outToConsole
-
-	if err != nil {
-		return nil, err
+func NewLogger(path string, outToConsole bool, outToFile bool) (*Logger, error) {
+	l := Logger{}
+	if outToFile {
+		logFile, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+		l.file = logFile
+		if err != nil {
+			return nil, err
+		}
 	}
+	l.outToConsole = outToConsole
+	l.outToFile = outToFile
 	return &l, nil
 }
 
-func NewMainLogger(path string, outToConsole bool) error {
-	logMain = &Logger{OutToFile: true}
-	logFile, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
-	logMain.file = logFile
-	logMain.outToConsole = outToConsole
-	if err != nil {
-		return err
+func NewMainLogger(path string, outToConsole bool, outToFile bool) error {
+	logMain = &Logger{}
+	if outToFile && path == "" {
+		return errors.New("To record the logger in the file, you must specify the path to the file")
 	}
+	if outToFile {
+		logFile, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+		logMain.file = logFile
+		if err != nil {
+			return err
+		}
+	}
+	logMain.outToConsole = outToConsole
+	logMain.outToFile = outToFile
+
 	return nil
 }
 
@@ -63,14 +72,26 @@ func Close() error {
 }
 
 func (l *Logger) LogInfo(format string, v ...interface{}) {
-	logInfo := l.returnLog(l.outToConsole, l.OutToFile, info)
+	if l == nil {
+		return
+	}
+	logInfo := l.returnLog(l.outToConsole, l.outToFile, info)
+	if logInfo == nil {
+		return
+	}
 	err := logInfo.Output(2, fmt.Sprintf(format, v...))
 	if err != nil {
 		fmt.Printf("ERROR: при записи LogInfo")
 	}
 }
 func (l *Logger) LogWarn(format string, v ...interface{}) {
-	logWarn := l.returnLog(l.outToConsole, l.OutToFile, warn)
+	if l == nil {
+		return
+	}
+	logWarn := l.returnLog(l.outToConsole, l.outToFile, warn)
+	if logWarn == nil {
+		return
+	}
 	err := logWarn.Output(2, fmt.Sprintf(format, v...))
 	if err != nil {
 		fmt.Printf("ERROR: при записи LogInfo")
@@ -78,7 +99,13 @@ func (l *Logger) LogWarn(format string, v ...interface{}) {
 }
 
 func (l *Logger) LogError(format string, v ...interface{}) {
-	logError := l.returnLogError(l.outToConsole, l.OutToFile)
+	if l == nil {
+		return
+	}
+	logError := l.returnLogError(l.outToConsole, l.outToFile)
+	if logError == nil {
+		return
+	}
 	err := logError.Output(2, fmt.Sprintf(format, v...))
 	if err != nil {
 		fmt.Printf("ERROR: при записи LogInfo")
@@ -86,33 +113,42 @@ func (l *Logger) LogError(format string, v ...interface{}) {
 }
 
 func LogInfo(format string, v ...interface{}) {
-	logInfo := logMain.returnLog(logMain.outToConsole, logMain.OutToFile, info)
+	logInfo := logMain.returnLog(logMain.outToConsole, logMain.outToFile, info)
 	if logMain == nil {
 		fmt.Println("ERROR : The main stream logger has not been initialized. Please call the MainLogger function")
+		return
+	}
+	if logInfo == nil {
 		return
 	}
 	err := logInfo.Output(2, fmt.Sprintf(format, v...))
 	if err != nil {
-		fmt.Printf("ERROR: при записи LogInfo")
+		fmt.Printf("ERROR: при записи LogInfo основного лога %v", err)
 	}
 }
 
 func LogWarn(format string, v ...interface{}) {
-	logWarn := logMain.returnLog(logMain.outToConsole, logMain.OutToFile, warn)
+	logWarn := logMain.returnLog(logMain.outToConsole, logMain.outToFile, warn)
 	if logMain == nil {
 		fmt.Println("ERROR : The main stream logger has not been initialized. Please call the MainLogger function")
 		return
 	}
+	if logWarn == nil {
+		return
+	}
 	err := logWarn.Output(2, fmt.Sprintf(format, v...))
 	if err != nil {
-		fmt.Printf("ERROR: при записи LogWarn основого лога")
+		fmt.Printf("ERROR: при записи LogWarn основого лога", err)
 	}
 }
 
 func LogError(format string, v ...interface{}) {
-	logError := logMain.returnLogError(logMain.outToConsole, logMain.OutToFile)
+	logError := logMain.returnLogError(logMain.outToConsole, logMain.outToFile)
 	if logMain == nil {
 		fmt.Println("ERROR : The main stream logger has not been initialized. Please call the MainLogger function")
+		return
+	}
+	if logError == nil {
 		return
 	}
 	err := logError.Output(2, fmt.Sprintf(format, v...))
@@ -122,15 +158,15 @@ func LogError(format string, v ...interface{}) {
 }
 
 func (l *Logger) returnLog(outToConsole, outToFile bool, level string) *log.Logger {
-	if l.outToConsole && l.OutToFile {
+	if l.outToConsole && l.outToFile && l.file != nil {
 		l.log = log.New(io.MultiWriter(l.file, os.Stdout), level, flags)
 		return l.log
 	}
-	if !l.OutToFile {
+	if !l.outToFile && l.outToConsole {
 		l.log = log.New(io.MultiWriter(os.Stdout), level, flags)
 		return l.log
 	}
-	if l.OutToFile && !l.outToConsole {
+	if l.outToFile && !l.outToConsole {
 		l.log = log.New(io.MultiWriter(l.file), level, flags)
 		return l.log
 	}
@@ -138,11 +174,11 @@ func (l *Logger) returnLog(outToConsole, outToFile bool, level string) *log.Logg
 }
 
 func (l *Logger) returnLogError(outToConsole, outToFile bool) *log.Logger {
-	if l.OutToFile {
+	if l.outToFile {
 		l.log = log.New(io.MultiWriter(l.file, os.Stderr), errLog, flags)
 	}
-	if !l.OutToFile {
-		l.log = log.New(io.MultiWriter(os.Stdout), errLog, flags)
+	if !l.outToFile {
+		l.log = log.New(io.MultiWriter(os.Stderr), errLog, flags)
 	}
 	return l.log
 }
